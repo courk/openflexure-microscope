courk/openflexure-microscope

commit c03ac6a1d6cd2759e85375caf62d679c2fcc0aad

Merged in optimisations from branch ams_stage
ams_stage improved a bunch of stuff (mostly optimising the compile time and neatening up the code) as well as introducing a pure XY stage.  This copies the optimisations back to master.  It draws on commits 43b8f7f78aba33056d2cf03070e6a4c7ba7da23b [formerly 846d39951b430c1ad5a11b1f9c536ad2f7e3e3c4] [formerly f3dc050afae1c7298b7abb166cb23c2c8e6c21c6] [formerly 7b62e433c26eb2185c034ac9ae543fa4be9dc9f4 [formerly c5e0187ca28f332e268baa50b574e2e69b248174]] [formerly 7c7500c54fc9cbf52a3ef2c5ddb133f76233e1e7 [formerly 82685edb6a11fb0578115c5529fde7a3ec472e1e] [formerly 2be1a354dadda54aa0f14ebc650256f036c140ab [formerly 9497b6a0eee9c9daeeef2d32287566f585bd224a]]], 4d5d70a141ab91a67514c02e4671c361843bf222 [formerly 7df7eee3f1edd261ab7e3e85523b0d0d8a095762] [formerly 38bac1601db430a68598d3c02e34e93191da7e60] [formerly c48280c1e124a9abba665ae495512e9bea4119da [formerly 47ac1c134ac29a63079dc7c8550c9c05d40a3ab1]] [formerly daae03e3dafaabb264d461eeec1f43b4f2f9c003 [formerly 91703dc2ccb817554a373dc68b750d94f31f2bc6] [formerly e90f4160794b445a8599c7350134c7c78fe35f12 [formerly 9718076b96af75b9cc325797532d4cfcf974fdfc]]], and 152467d497d0d8eedf71d05aa824ba0ea8a6f685 [formerly b64933f6babb744ab713e819446fd254b4d82f41] [formerly ead32fb1d2c9f1bf84bb1e11f72163595c51f793] [formerly 1bad55f1e04fcfa3ab97207f53a1fbd0d24f915e [formerly 6bc65991e4b0d66e05cdbb002b17a510cecb4494]] [formerly ef298daf790107ec068ec23326ed8059a73195ef [formerly cf5088d39f5ec0eb255d0b47966da87a9255fda3] [formerly 543b7d8063ddddbabdc56ae05e402a25561a59c0 [formerly 2e2d18ada9295876f716dcb1b0006bd60482f017]]]


Former-commit-id: c19267f5dd89fc566ed04489cfdf57cbfa9c64e1 [formerly e874da0a2599b9baac8a901b9d2be782becd951b] [formerly 6fba209802396fb0ab6b5155686d72d577b127bf [formerly 9ec54116617bfd79eea0cb6efe4d929fe2c06a28]]
Former-commit-id: 4c37fb0409b94b991826274146f0579df80ad7c8 [formerly d17ba0873a7e23fafe627c36e1e56123c21cbe78]

openscad/main_body.scad
@@ -66,7 +66,7 @@ module leg(brace=stage_flex_w){
 }
 module actuator(){
     // A leg that supports the stage, plus a lever to tilt it.
-    // Includes the flexible nut seat actuating column.
+    // No longer includes the flexible nut seat actuating column.
     // TODO: find the code that unifies this with leg()
 	brace=20;
     fw=stage_flex_w;
@@ -84,8 +84,6 @@ module actuator(){
             //don't foul the actuator column
             translate([0,actuating_nut_r,0]) actuator_end_cutout(); 
         }
-		//nut seat
-		translate([0,actuating_nut_r,0]) actuator_column(h=actuator_h); 
 	}
 }
 module actuator_silhouette(h=999){
@@ -93,12 +91,7 @@ module actuator_silhouette(h=999){
     // actuators.
     linear_extrude(2*h,center=true) minkowski(){
         circle(r=zflex_l,$fn=12);
-        projection() difference(){
-            actuator();
-            // cut off the actuator column - this causes problems and
-            // the inside of the screw seat is already chopped out...
-            translate([0,actuating_nut_r,0]) actuator_end_cutout(); 
-        }
+        projection() actuator();
     }
 }
 
@@ -227,9 +220,7 @@ module wall_vertex(r=wall_t/2, h=wall_h, x_tilt=0, y_tilt=0){
     // (i.e. it's sheared rather than tilted).    These form the
     // stiffening "wall" that runs around the base of 
     // the legs
-    hull() repeat([tan(y_tilt), -tan(x_tilt), 1]*(h-d), 2){
-        cylinder(r=r, h=d, $fn=8);
-    }
+    smatrix(xz=tan(y_tilt), yz=-tan(x_tilt)) cylinder(r=r, h=h, $fn=8);
 }
 module inner_wall_vertex(leg_angle, x, h=wall_h, y_tilt=-999, y=-zflex_l-wall_t/2){
     // A thin cylinder, close to one of the legs.  It
@@ -278,9 +269,12 @@ module place_on_wall(){
 ///////////////////// MAIN STRUCTURE STARTS HERE ///////////////
 union(){
 
-	//legs
+	//legs (incl. actuators)
 	reflect([1,0,0]) leg_frame(135) leg();
-	each_actuator() actuator();
+	each_actuator(){
+        actuator();
+		translate([0,actuating_nut_r,0]) actuator_column(h=actuator_h);
+    }
 	//flexures connecting bottoms of legs to centre
 	each_leg() reflect([1,0,0]) translate([0,0,flex_z1]){
         w=stage_flex_w;

openscad/actuator_drilling_jig.scad
@@ -1,0 +1,43 @@
+/******************************************************************
+*                                                                 *
+* OpenFlexure Microscope: actuator column drilling jig            *
+*                                                                 *
+* If you need to drill out the hole for the M3 screw, it's not    *
+* hard to snap the flexure at the bottom of the actuator column.  *
+* This tool is inserted from the bottom of the microscope to stop *
+* the column rotating, and hopefully avoid snapping the flexure.  *
+* To use it, slide it in from the bottom.  Then, when you drill   *
+* out the hole, hold the tool and *not* the body of the           *
+* microscope, so no torque goes through the microscope body.      *
+* An M4 screw can be used to mount the tool to a workbench.       *
+*                                                                 *
+* (c) Richard Bowman, January 2016                                *
+* Released under the CERN Open Hardware License                   *
+*                                                                 *
+******************************************************************/
+
+use <./utilities.scad>;
+use <./compact_nut_seat.scad>;
+use <./logo.scad>;
+use <./dovetail.scad>;
+include <./microscope_parameters.scad>; //All the geometric variables are now in here.
+
+outer_clearance = 0.5;
+cr = column_base_radius() + outer_clearance;
+
+difference(){
+    translate([0,0,-7]) linear_extrude(actuator_h+5) offset(-outer_clearance) projection(cut=true) nut_seat_void();
+    
+    //void for the actuator column
+    minkowski(){
+        actuator_column(h=actuator_h+1, no_voids=true, flip_nut_slot=true);
+        cylinder(r=0.5, h=d, $fn=8);
+    }
+    //clearance for the lever
+    translate([-cr,0,0]) mirror([0,1,0]) cube([cr*2,999,999]);
+    //clearance for the column core
+    cylinder(r=cr, $fn=16, h=999);
+    //mounting bolt
+    translate([0,0,-4])cylinder(r=4,h=6);
+    cylinder(r=2.6,h=999,center=true);
+}

openscad/compact_nut_seat.scad
@@ -55,10 +55,16 @@ module nut_trap_and_slot(r, slot, squeeze=0.9, trap_h=-1){
         
 }
 
-module actuator_column(h, tilt=0, lever_tip=3, flip_nut_slot=false, join_to_casing=false){
+module actuator_column(h, tilt=0, lever_tip=3, flip_nut_slot=false, join_to_casing=false, no_voids=false){
     // An "actuator column", a nearly-vertical tower, with a nut trap and hooks
     // for elastic bands at the top, usually attached to a flexure at the bottom.
     // There's often one of these inside the casing under an adjustment screw/gear
+    //h: height of the column
+    //tilt: the column is rotated about the x axis
+    //lever_tip: height of the actuating lever at its end (can taper up at 45 degrees)
+    //flip_nut_slot: if set to true, the nut is inserted from -y
+    //join_to_casing: if set to true, the column is joined to the casing by thin threads
+    //no_voids: don't leave a void for the nut or screw, used for the drilling jig.
     r1 = column_base_r; //size of the bottom part
     top = nut_slot + [3,3,nut_size + 1.5]; //size of the top part
     r2 = sqrt(top[0]*top[0]+top[1]*top[1])/2; //outer radius of top
@@ -93,11 +99,12 @@ module actuator_column(h, tilt=0, lever_tip=3, flip_nut_slot=false, join_to_casi
         }
         
         // nut trap
-        rotate([tilt,0,0]) rotate(slot_angle) translate([0,0,h-top[2]]) nut_trap_and_slot(nut_size, nut_slot);
+        if(!no_voids) rotate([tilt,0,0]) rotate(slot_angle) 
+            translate([0,0,h-top[2]]) nut_trap_and_slot(nut_size, nut_slot);
         
         // shaft for the screw
         // NB this is raised up from the bottom so it stays within the shaft - this may need to change depending on the length of screw we use...
-        rotate([tilt,0,0]) translate([0,0,lever_tip]){
+        if(!no_voids) rotate([tilt,0,0]) translate([0,0,lever_tip]){
             cylinder(r=shaft_r, h=999);
             translate([0,0,-lever_tip+1]) cylinder(r1=0, r=shaft_r, h=lever_tip-1); //pointy bottom (stronger)
         }
@@ -127,6 +134,16 @@ module actuator_end_cutout(lever_tip=3-0.5 ){
     }
 }
 
+module nut_seat_silhouette(r=ss_outer()[1]/2, dx=ss_outer()[0]-ss_outer()[1], offset=0){
+    // a (2D) shape made from the convex hull of two circles
+    //    hull() reflect([1,0]) translate([x,0]) circle(r=r);
+    // we don't actually build it like that though, as the hull is a slow operation...
+    union(){
+        reflect([1,0]) translate([dx/2,0]) circle(r=r+offset);
+        square([dx,2*(r+offset)], center=true);
+    }
+}
+
 module nut_seat_void(h=1, tilt=0, center=true){
     // Inside of the actuator column housing (should be subtracted
     // h is the height of the top (excluding nut hole)
@@ -135,7 +152,7 @@ module nut_seat_void(h=1, tilt=0, center=true){
     r = column_core[1]/2;
     x = column_core[0]/2 - r;
     rotate([tilt,0,0]) intersection(){
-        hull() reflect([1,0,0]) translate([x,0,0]) cylinder(r=r,h=999,center=center);
+        linear_extrude(999,center=center) nut_seat_silhouette(offset=-wall_t);
         translate([0,0,h]) rotate(90) hole_from_bottom(nut_size*1.1/2, h=999, base_w=999);
     }
 }
@@ -147,12 +164,11 @@ module screw_seat_shell(h=1, tilt=0){
     // (see screw_seat)
     r = ss_outer(h)[1]/2;
     x = ss_outer(h)[0]/2 - r;
+    double_h = ss_outer(h)[2];
     difference(){
-        rotate([tilt,0,0]) resize(ss_outer(h)) hull(){
-            reflect([1,0,0]) translate([x,0,0]){
-                cylinder(r=r,h=(h+0.5)*2,center=true);
-                cylinder(r=max(r-3,3.5),h=(h+2)*2,center=true);
-            }
+        rotate([tilt,0,0]) hull(){
+            linear_extrude(double_h-3, center=true) nut_seat_silhouette();
+            linear_extrude(double_h, center=true) nut_seat_silhouette(offset=-2);
         }
         mirror([0,0,1]) cylinder(r=999,h=999,$fn=8); //ground
         // hole through which we can insert the nut
@@ -160,12 +176,12 @@ module screw_seat_shell(h=1, tilt=0){
     }
 }
 
-module motor_lugs(h=20, tilt=0){
+module motor_lugs(h=20, tilt=0, angle=0){
     // lugs to mount a micro geared stepper motor on a screw_seat.
     motor_shaft_pos=[0,-20,h+2]; //see height of screw_seat_shell above
     motor_screw_pos=[35/2,motor_shaft_pos[1]+7.8,motor_shaft_pos[2]+10];
     screw_r = sqrt(pow(motor_screw_pos[0],2)+pow(motor_screw_pos[1],2));
-    rotate([tilt,0,0]) reflect([1,0,0]) difference(){
+    rotate([tilt,0,0]) rotate(angle) reflect([1,0,0]) difference(){
         union(){
             hull(){
                 translate(motor_screw_pos-[0,0,8]) cylinder(r=4,h=8);
@@ -176,13 +192,13 @@ module motor_lugs(h=20, tilt=0){
         translate([0,0,h]) cylinder(r1=8,r2=17,h=2+d);
         translate([0,0,h+2]) cylinder(h=999,r=17);
         //hollow inside of the structure
-        nut_seat_void(h=h, tilt=tilt);
+        rotate(-angle) nut_seat_void(h=h, tilt=tilt);
         //mounting screws
         translate(motor_screw_pos) cylinder(r=1.9,h=20,center=true);
     }
 }
 
-module screw_seat(h=25, travel=5, entry_w=2*column_base_r+3, extra_entry_h=7, motor_lugs=false){
+module screw_seat(h=25, travel=5, entry_w=2*column_base_r+3, extra_entry_h=7, motor_lugs=false, lug_angle=0){
     // This forms a hollow column, usually built around an actuator_column to
     // support the screw (see screw_seat_shell)
     tilt = 0; //currently, only vertical ones are supported.
@@ -190,7 +206,7 @@ module screw_seat(h=25, travel=5, entry_w=2*column_base_r+3, extra_entry_h=7, mo
     difference(){
         union(){
             screw_seat_shell(h=h + travel);
-            if(motor_lugs) rotate(180) motor_lugs(h=h + travel);
+            if(motor_lugs) rotate(180) motor_lugs(h=h + travel, angle=lug_angle);
         }
         nut_seat_void(h=h + travel); //hollow out the inside
         
@@ -208,7 +224,7 @@ module screw_seat_outline(h=999,adjustment=0,center=false){
     //l = ss_outer()[1];
     //a = adjustment;
 	//resize([w+a, l+a, h]) cylinder(r=20, h=h, center=center);
-    linear_extrude(h,center=center) offset(adjustment) projection(cut=true) translate([0,0,-1]) screw_seat_shell();
+    linear_extrude(h,center=center) nut_seat_silhouette(offset=adjustment); //offset(adjustment) projection(cut=true) translate([0,0,-1]) screw_seat_shell();
 }
 
 
@@ -365,10 +381,10 @@ translate([40,0,0]){
 //    tilted_actuator(25,25,50, base_w=6);
 }
 //echo(nut_slot);
-/*/
+//
 difference(){
     union(){
-        screw_seat(25, motor_lugs=false);
+        screw_seat(25, motor_lugs=true);
 
         difference(){ //an example actuator rod
             translate([-3,-40,0]) cube([6,40,5]);

openscad/feet.scad
@@ -155,5 +155,5 @@ module foot(travel=5,
 //foot(tilt=15);
 //foot(tilt=0,hover=2);
 
-reflect([0,1,0]) translate([0,ss_outer()[1]+3.5, 0]) foot(tilt=15, lie_flat=true);
+reflect([0,1,0]) translate([0,ss_outer()[1]+foot_height*sin(15)+0.5, 0]) foot(tilt=15, lie_flat=true);
 foot(tilt=0,hover=2, lie_flat=true);

commit 83176dbbe70fc74d1bc4edb32bbf7f5e5e9cf329

rebuilt everything
The main body had a bug (introduced in c03ac6a1d6cd2759e85375caf62d679c2fcc0aad [formerly 58f9744ec019c9dfb411169ec076ae25674016b1] [formerly a17aeac8fe746c71ecec0c8a34ffb27c137192ac] [formerly 4c37fb0409b94b991826274146f0579df80ad7c8 [formerly d17ba0873a7e23fafe627c36e1e56123c21cbe78]] [formerly c19267f5dd89fc566ed04489cfdf57cbfa9c64e1 [formerly e874da0a2599b9baac8a901b9d2be782becd951b] [formerly 6fba209802396fb0ab6b5155686d72d577b127bf [formerly 9ec54116617bfd79eea0cb6efe4d929fe2c06a28]]]) where two of the posts that make the wall protruded below z=0.  This is now fixed.


Former-commit-id: 65766e6339eed65fb0498640b8d09852b52d1006 [formerly ad862e26aacc6972c209df9048d52bd9a7fce182] [formerly b642fe6867aa57fcaffc917ddb4e4d96b960b0f7 [formerly 5a82f859d5b4529c41948ddec5cd0bcaacae13b0]]
Former-commit-id: 553f38aa73c0b11606edafe69df484414b249466 [formerly 6e6d76deccbcec80a8c0d7c4ed2a919a15c5d38f]
Former-commit-id: bb446b93d2e95a92b9d72828289dfb54e6920735
Former-commit-id: f19ae189b211f5b919c5f07e10dfb76de3ff542e

openscad/actuator_assembly_tools.scad
@@ -190,5 +190,6 @@ module double_ended_band_tool(bent=false){
         translate([0,0,roc]) cube([ns[0],middle_w+2*(roc-0.5),d],center=true);
     }
 }
+
 double_ended_band_tool(bent=false);
-//translate([10,0,0]) nut_tool();
+translate([10,0,0]) nut_tool();

openscad/main_body.scad
@@ -346,8 +346,8 @@ union(){
                 }
                 // Finally, link the actuators together
                 reflect([1,0,0]) hull(){
-                    leg_frame(45) translate([ss_outer()[0]/2-1,actuating_nut_r,-d]) cylinder(r=1,h=wall_h,$fn=8);
-                    translate([0,z_nut_y+ss_outer()[1]/2-1,-d]) cylinder(r=1,h=wall_h,$fn=8);
+                    leg_frame(45) translate([ss_outer()[0]/2-wall_t/2,actuating_nut_r,0]) wall_vertex();
+                    translate([0,z_nut_y+ss_outer()[1]/2-wall_t/2,0]) wall_vertex();
                 }
                 // add a small object to make sure the base is big enough
                 wall_vertex(h=base_t);

rebuild_accessories.bat
@@ -4,3 +4,6 @@ rem "compiling feet, gears, and sample clips"
 "C:\Program Files\OpenSCAD\openscad.com" -o builds/feet_tall.stl -D foot_height=26  OpenSCAD/feet.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o builds/gears.stl OpenSCAD/gears.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o builds/sample_clips.stl OpenSCAD/sample_clips.scad
+
+rem "compiling elastic band tools"
+"C:\Program Files\OpenSCAD\openscad.com" -o builds/actuator_assembly_tools.stl OpenSCAD/actuator_assembly_tools.scad

rebuild_all.bat
@@ -1,0 +1,6 @@
+rem "This batch file recompiles the 6 standard versions of the microscope, including everything but the optics modules."
+
+rebuild_illumination.bat
+rebuild_accessories.bat
+rebuild_body_x6.bat
+

rebuild_body_x6.bat
@@ -7,6 +7,6 @@ rem "This batch file recompiles the four standard versions of the microscope."
 "C:\Program Files\OpenSCAD\openscad.com" -o builds/body_LS75.stl -D motor_lugs=false -D big_stage=true -D sample_z=75 OpenSCAD/main_body.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o builds/body_LS75-M.stl -D motor_lugs=true -D big_stage=true -D sample_z=75 OpenSCAD/main_body.scad
 
-rebuild_illumination.bat
-rebuild_accessories.bat
+rem rebuild_illumination.bat
+rem rebuild_accessories.bat
 