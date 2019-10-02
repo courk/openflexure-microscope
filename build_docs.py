348047f02fbf0e740acd71fc74802a8bfc3833fb
Joel Collins <joel@jtcollins.net>
2019-10-02 15:24:14 +0100

    Create docs index using docsify

--- build_docs.py
+++ build_docs.py
@@ -6,29 +6,14 @@ import shutil
 import re
 
 def process_markdown(infile, outfile):
-    """Remove the title of a markdown file, and add Jekyll front matter"""
-    preamble_lines = []
+    """Process and copy a markdown file, and scan for images used in the file"""
     images = set()
-    extracted_title = False
     with open(infile, 'r', encoding='utf8') as input_file, open(outfile, 'w', encoding='utf8') as output_file:
         print(f"Opened {infile}...")
         for line in input_file:
-            if not extracted_title:
-                if line.startswith("# "):
-                    # once we have the title, write the YAML block, then regurgitate the preceding bit of the file.
-                    title = line.strip("# \n")
-                    output_file.write("---\n")
-                    output_file.write("layout: page\n")
-                    output_file.write(f"title: {title}\n")
-                    output_file.write("nav: false\n")
-                    output_file.write("---\n")
-                    output_file.writelines(preamble_lines)
-                    extracted_title = True
-                    print(f"Added header for '{title}' ({infile})")
-                else:
-                    preamble_lines.append(line)
-            else:
-                output_file.write(line) # copy over the file
+            # Copy over the line
+            output_file.write(line) # copy over the file
+            # Search for images
             m = re.search(r"images/([^.]*\.(jpeg|jpg|JPG|JPEG|png|PNG))", line)
             if m:
                 images.add(m.group(1))
@@ -38,6 +23,7 @@ def process_markdown(infile, outfile):
     
 
 if __name__ == "__main__":
+    # Find all relevant directories
     here = os.path.dirname(os.path.realpath(__file__))
     docs_dir = os.path.abspath(os.path.join(here, "docs"))
     parts_dir = os.path.abspath(os.path.join(docs_dir, "parts"))
@@ -57,8 +43,12 @@ if __name__ == "__main__":
             os.mkdir(os.path.join(output_dir, "parts", f))
     os.mkdir(os.path.join(output_dir, "images"))
 
+    # Copy our docsify index page
+    shutil.copyfile(os.path.join(docs_dir, "index.html"), os.path.join(output_dir, "index.html"))
+
+    # Create a set to note our used image files
     images = set()
-    # This should be recursive or something - but for now, I just enumerate the parts directory
+    # TODO: This should be recursive or something - but for now, I just enumerate the parts directory
     for input_dir in ['.', 'parts'] + [os.path.join('parts', d) for d in os.listdir(parts_dir) if os.path.isdir(os.path.join(parts_dir, d))]:
         input_dir_abs = os.path.join(docs_dir, input_dir)
         for f in os.listdir(os.path.join(docs_dir, input_dir)):
@@ -66,7 +56,7 @@ if __name__ == "__main__":
                 f_images = process_markdown(os.path.join(input_dir_abs, f), os.path.join(output_dir, input_dir, f))
                 images.update(f_images)
 
-    # copy over only the images that are actually used
+    # Copy over only the images that are actually used
     for f in images:
         shutil.copyfile(os.path.join(docs_dir, "images", f), os.path.join(output_dir, "images", f))
                 
