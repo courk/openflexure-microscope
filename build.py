f49c3a086268d8911347f35f6168cf906e678a01
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-10-08 16:37:18 +0100

    Switch to using list for parameters

--- build.py
+++ build.py
@@ -18,19 +18,19 @@ body_versions = [
 
 
 for size, motors, brim in body_versions:
-    parameters = "-D 'big_stage=true'"
+    parameters = ["-D 'big_stage=true'"]
 
     if brim == "":
-        parameters += " -D 'enable_smart_brim=false'"
+        parameters.append("-D 'enable_smart_brim=false'")
     else:
-        parameters += " -D 'enable_smart_brim=true'"
+        parameters.append("-D 'enable_smart_brim=true'")
 
-    parameters += " -D 'sample_z={}'".format(size)
+    parameters.append("-D 'sample_z={}'".format(size))
 
     if motors == "":
-        parameters += " -D 'motor_lugs=false'"
+        parameters.append("-D 'motor_lugs=false'")
     else:
-        parameters += " -D 'motor_lugs=true'"
+        parameters.append("-D 'motor_lugs=true'")
 
     output = "build/main_body_LS" + size + motors + brim + ".stl"
 
@@ -38,7 +38,7 @@ for size, motors, brim in body_versions:
         output,
         rule="openscad",
         inputs="openscad/main_body.scad",
-        variables={"parameters": parameters},
+        variables={"parameters": " ".join(parameters)},
     )
 
 
